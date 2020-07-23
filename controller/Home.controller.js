b2a772d4b4d3c97615ceb202bddd46fa79e08762
Ferry Djaja <ferrygun@yahoo.com>
2020-07-23 08:43:22 +0800

    Update Home.controller.js

--- controller/Home.controller.js
+++ controller/Home.controller.js
@@ -7,7 +7,7 @@ sap.ui.define([
     "use strict";
     var socket;
 
-    socket = io('https://f86a0fb1cd78.ngrok.io');
+    socket = io('https://f86a0fb1cd78.ngrok.io', { transport : ['websocket'] });
     console.log(socket);
 
     socket.on('req', function(data) {
